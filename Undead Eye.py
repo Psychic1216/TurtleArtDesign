444cc79c4b4550d1bb8864058bb97f3068a33059
TimothyNg5808 <33286218+TimothyNg5808@users.noreply.github.com>
2017-11-03 08:40:25 -0400

    Design

---
+++ Undead Eye.py
@@ -0,0 +1,16 @@
+import turtle
+from TNG_MyShape import *
+from ProjectFunctions import *
+from EyeFunctions import *
+turtle.colormode(255)
+turtle.bgcolor("black")
+
+bob=turtle.Turtle()
+bob.speed(0)
+
+sclera(bob,86)
+
+cornea(bob,25,65)
+
+pupil(bob,10,65)
+
